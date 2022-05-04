b07e10044f83cfea6bc27629cfc05a4500fec447
CAMHub PublishBot <octravis@us.ibm.com>
2022-05-04 17:04:36 +0000

    Automated CAMHub::publish on 2022-05-04 17:04:36 UTC

--- terraform/modules/helm_tiller/main.tf
+++ terraform/modules/helm_tiller/main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "tiller" {
     cluster_config = var.cluster_config
   }
 
-  # helm init
+  
   # helm init
   provisioner "local-exec" {
     command = "chmod 755 ${path.module}/scripts/init_tiller.sh && ${path.module}/scripts/init_tiller.sh"
@@ -21,15 +21,15 @@ resource "null_resource" "tiller" {
     }
   }
 
-  # helm reset
+  
   # helm reset
   provisioner "local-exec" {
     command = "chmod 755 ${path.module}/scripts/reset_tiller.sh && ${path.module}/scripts/reset_tiller.sh"
     environment = {
-      CLUSTER_NAME                  = self.cluster_name
-      HELM_VERSION                  = self.helm_version
-      CLUSTER_CERTIFICATE_AUTHORITY = base64decode(self.cluster_certificate_authority)
-      CLUSTER_CONFIG                = base64decode(self.cluster_config)
+      CLUSTER_NAME                  = self.triggers.cluster_name
+      HELM_VERSION                  = self.triggers.helm_version
+      CLUSTER_CERTIFICATE_AUTHORITY = base64decode(self.triggers.cluster_certificate_authority)
+      CLUSTER_CONFIG                = base64decode(self.triggers.cluster_config)
       SCRIPTS_PATH                  = "${path.module}/scripts"
     }
     when = destroy
